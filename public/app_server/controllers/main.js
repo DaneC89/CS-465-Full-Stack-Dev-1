2da032c03e2e8b4125339ac23f3a6045e78c6c20
DaneC89 <dane.clark89@gmail.com>
2023-07-08 01:33:07 -0500

    Deleted Leftover Files

--- public/app_server/controllers/main.js
+++ public/app_server/controllers/main.js
@@ -1,5 +1,4 @@
 /* GET homepage */
-
 const index = (req, res) => {
     console.log('Inside app_server, controllers, main.js, index function.');
     res.render('index', { title: 'Travlr Gateways' });
