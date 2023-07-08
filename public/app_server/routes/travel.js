0811b9ad559c98c47f4be3a4b513bb1cd01e3133
DaneC89 <dane.clark89@gmail.com>
2023-07-08 02:02:12 -0500

    Fixing errors

--- public/app_server/routes/travel.js
+++ public/app_server/routes/travel.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller= require('../controllers/travel');
 
 /* GET home page. */
-console.log('Inside app_server, routes, travel.js.')
+console.log('Inside app_server, routes, travel.js');
 router.get('/', controller.travel);
 
 module.exports = router;
