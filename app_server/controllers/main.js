14b29a7daa1b9af38025172447fc38c455c0ecf0
DaneC89 <dane.clark89@gmail.com>
2023-08-17 20:41:06 -0500

    Final Project

---
+++ app_server/controllers/main.js
@@ -0,0 +1,9 @@
+/* GET homepage */
+const index = (req, res) => {
+    console.log('Inside app_server, controllers, main.js, index function.');
+    res.render('index', { title: 'Travlr Gateways' });
+};
+
+module.exports = {
+    index
+};
